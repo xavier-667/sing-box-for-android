d396bd606a424651ab1ea636419cef7ef60bb49f
xavier-667 <itexpress224@gmail.com>
2026-06-10 14:15:10 +0200

    Change package name from io.nekohasekai.sfa to io.surprise.sfa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ fun getVersionProps(propName: String): String {
 }
 
 android {
-    namespace = "io.nekohasekai.sfa"
+    namespace = "io.surprise.sfa"
     compileSdk = 36
 
     ndkVersion = "28.0.13004108"
@@ -67,7 +67,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "io.nekohasekai.sfa"
+        applicationId = "io.surprise.sfa"
         minSdk = 21
         targetSdk = 35
         versionCode = getVersionProps("VERSION_CODE").toInt()
